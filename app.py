bc30d51763bb588720e15c83b03c961d56b66a4c
Oreva12 <ovedheo@gmail.com>
2025-02-09 16:15:24 +0100

    Stage 1 done!

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def classify_number():
         return jsonify({"error": True, "message": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
